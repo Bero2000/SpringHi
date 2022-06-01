b6f681146dda17df65571b3409fe412d9622f1ac
Berislav <berislav.adzaga2@gmail.com>
2022-06-02 01:06:03 +0200

    Bero

--- src/main/java/com/tvrtko/hi/controllers/HiController.java
+++ src/main/java/com/tvrtko/hi/controllers/HiController.java
@@ -2,13 +2,17 @@ package com.tvrtko.hi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @RestController
 public class HiController {
     
-    @GetMapping(value = "/hi")
-    public String SayHi() {
-        return ("Hi man/woman!");
+    @GetMapping(value = "/hey")
+    
+    public String SayHi(@RequestParam(defaultValue = "man/woman") String name) {
+        return "Hi " + name;
     }
 
 }
